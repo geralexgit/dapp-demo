3955785ebfc9bcb42845a40c5721eb3029402fbf
Alexander Gerasimov <a.gerasimov@itsl.ru>
2018-08-27 12:36:49 +0300

    candidates added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Constructor
-    // Read candidate
-    string public candidate;
-    // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
+
+    // Modela a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
+
+    // Store Candidates
+    
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidadte Count
+    uint public candidatesCount;
+
+    function Election () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
