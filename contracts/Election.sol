40c422e37f5485100e3dac3ec8eb48e1b0ffa46f
Alexander Gerasimov <a.gerasimov@itsl.ru>
2018-08-27 12:11:39 +0300

    project inited, first migrations added

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+contract Election {
+    // Constructor
+    // Read candidate
+    string public candidate;
+    // Constructor
+    function Election () public {
+        candidate = "Candidate 1";
+    }
+}
